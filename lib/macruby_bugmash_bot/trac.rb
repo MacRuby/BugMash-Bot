e40354d9bf9018cfe6c8f6cd8fcb6ba3cc77f13e
Eloy Duran <eloy.de.enige@gmail.com>
2010-12-03 12:49:26 +0100

    Return a open ticket in ascending order.

--- lib/macruby_bugmash_bot/trac.rb
+++ lib/macruby_bugmash_bot/trac.rb
@@ -1,21 +1,28 @@
-# RSS: http://www.macruby.org/trac/query?status=new&status=reopened&format=rss&col=id&col=summary&col=status&col=time&order=priority&max=1000
-
 $:.unshift File.expand_path("../../../vendor/simple-rss/lib", __FILE__)
 require "simple-rss"
 require "cgi"
 
-module Trac
+class Trac
+  # RSS feed: http://www.macruby.org/trac/query?status=new&status=reopened&format=rss&col=id&col=summary&col=status&col=time&order=priority&max=1000
   def self.raw_active_tickets_feed
     # TODO
   end
 
-  def self.active_tickets
-    rss = SimpleRSS.parse(raw_active_tickets_feed)
-    result = rss.entries.inject({}) do |h, entry|
-      id = File.basename(entry[:link]).to_i
-      h[id] = { :link => entry[:link], :summary => CGI.unescapeHTML(entry[:title]) }
-      h
+  def active_tickets
+    @active_tickets ||= begin
+      rss = SimpleRSS.parse(self.class.raw_active_tickets_feed)
+      result = rss.entries.inject({}) do |h, entry|
+        id = File.basename(entry[:link]).to_i
+        h[id] = { :id => id, :link => entry[:link], :summary => CGI.unescapeHTML(entry[:title]) }
+        h
+      end
+      result
     end
-    result
+  end
+
+  # Returns a ticket that nobody is working on yet, in ascending order.
+  def open_ticket
+    id = active_tickets.keys.sort.first
+    active_tickets[id]
   end
 end
--- spec/trac_spec.rb
+++ spec/trac_spec.rb
@@ -13,15 +13,25 @@ def fixture_read(name)
   File.read(fixture(name))
 end
 
-module Trac
+class Trac
   def self.raw_active_tickets_feed
     fixture_read("trac_active_tickets_rss.xml")
   end
 end
 
 describe "Trac" do
+  before do
+    @trac = Trac.new
+  end
+
   it "parses the raw RSS feed" do
-    Trac.active_tickets[189][:summary].should == "#189: Bugs with: Class#dup & Object#dup"
-    Trac.active_tickets[105][:summary].should == "#105: BridgeSupport can't convert KCGSessionEventTap as an argument for CGEventTapCreate"
+    @trac.active_tickets[189][:id].should == 189
+    @trac.active_tickets[189][:summary].should == "#189: Bugs with: Class#dup & Object#dup"
+    @trac.active_tickets[105][:id].should == 105
+    @trac.active_tickets[105][:summary].should == "#105: BridgeSupport can't convert KCGSessionEventTap as an argument for CGEventTapCreate"
+  end
+
+  it "returns a ticket that nobody is working on yet, in ascending ID order" do
+    @trac.open_ticket[:id].should == 19
   end
 end
